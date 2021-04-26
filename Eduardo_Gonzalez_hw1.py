c233076759403e6c11c365487355b51523421a6e
Eduardo-Gonz <erugonz249@gmail.com>
2021-04-25 18:55:31 -0700

    Add code that validates that user input is greater than zero.

--- Eduardo_Gonzalez_hw1.py
+++ Eduardo_Gonzalez_hw1.py
@@ -7,18 +7,21 @@
 def validate_input():
     while True:
         try:
-            yearInput = int(input("Enter a year: "))
+            userInput = int(input("Enter a year: "))
         except ValueError:
             print("That is not an integer! Please try again")
             continue
         else:
-            #validateRange(yearInput)
-            break
+            if(userInput <= 0):
+                print("Enter a year that is greater than zero")
+            else:
+                break
 
-    is_leap_year(yearInput)
+    is_leap_year(userInput)
+
+
 
 def is_leap_year(year):
-
     if(year % 4 == 0):
         if(year % 100 == 0):
             if(year % 400 == 0):
