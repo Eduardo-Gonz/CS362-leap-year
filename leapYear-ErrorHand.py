6e3be396d84072b81269d9eb8347cd1d8bc8959a
Eduardo-Gonz <erugonz249@gmail.com>
2021-04-25 19:40:46 -0700

    Split program up into two programs. One contains error handling and the other does not

---
+++ leapYear-ErrorHand.py
@@ -0,0 +1,40 @@
+# Directions to run this file:
+# 1. Open terminal
+# 2. Navigate to directory that this file is in
+# 3. Run this command in terminal: "python Eduardo_Gonzalez_hw1.py"
+#Note: This program is meant to run on the latest version of Python.
+
+
+def validate_input():
+    while True:
+        try:
+            userInput = int(input("Enter a year: "))
+        except ValueError:
+            print("That is not an integer! Please try again")
+            continue
+        else:
+            if(userInput <= 0):
+                print("Enter a year that is greater than zero")
+            else:
+                break
+
+    is_leap_year(userInput)
+
+
+
+def is_leap_year(year):
+    if(year % 4 == 0):
+        if(year % 100 == 0):
+            if(year % 400 == 0):
+                print("{0} is a leap year !".format(year))
+            else:
+                print("{0} is not a leap year".format(year))
+        else:
+            print("{0} is a leap year !".format(year))
+    else:
+        print("{0} is not a leap year".format(year))
+
+validate_input()
+
+
+
--- leapYear-NoErrorHand.py
+++ leapYear-NoErrorHand.py
@@ -2,26 +2,14 @@
 # 1. Open terminal
 # 2. Navigate to directory that this file is in
 # 3. Run this command in terminal: "python Eduardo_Gonzalez_hw1.py"
-
-
-def validate_input():
-    while True:
-        try:
-            userInput = int(input("Enter a year: "))
-        except ValueError:
-            print("That is not an integer! Please try again")
-            continue
-        else:
-            if(userInput <= 0):
-                print("Enter a year that is greater than zero")
-            else:
-                break
-
-    is_leap_year(userInput)
+#Note: This program is meant to run on the latest version of Python.
 
 
 
-def is_leap_year(year):
+
+def is_leap_year():
+    year = int(input("Enter a year: "))
+
     if(year % 4 == 0):
         if(year % 100 == 0):
             if(year % 400 == 0):
@@ -33,7 +21,7 @@ def is_leap_year(year):
     else:
         print("{0} is not a leap year".format(year))
 
-validate_input()
+is_leap_year()
 
 
 
